729925a0530df508df26c5dcd50f538a648ef1d0
Arjun Khurana <arjunkh99@gmail.com>
2020-05-29 13:45:29 -0500

    added argparser

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pandas as pd
+import numpy as np
 import sys
 import os
 from tqdm import tqdm
+import datetime
+import argparse
 
-dfs = []
+parser = argparse.ArgumentParser()
+parser.add_argument('inputDir', \
+    help='Directory where input CSVs are located')
+parser.add_argument('-a', '--appended', \
+    help='Outputs intensity data as columns in MegaCSV',
+    action='store_true')
+parser.add_argument('-s', '--separated', \
+    help='Outputs intensity data as separate CSV',
+    action='store_true')
+args = parser.parse_args()
 
 def fileparse(filename):
     split = (filename.replace('.csv','').split())
@@ -27,24 +39,49 @@ def fileparse(filename):
         'posY': posY
     }
 
-def rowAdd(row, file, info):
-    row['filename'] = file
-    row['wafer'] = info['wafer']
+def stdDev(intensity):
+    return {
+        'count': len(intensity),
+        'sum': np.sum(intensity),
+        'mean': np.mean(intensity),
+        'sigma': np.std(intensity)
+    }
 
-for file in tqdm(os.listdir(sys.argv[1])):
+dfs = []
+std = pd.DataFrame(columns=['posX', 'posY', 'count', 'sum', 'mean', 'sigma'])
+
+for file in tqdm(os.listdir(args.inputDir)):
     if file.endswith('.csv'):
-        filename = os.path.join(sys.argv[1], file)
-        info = fileparse(file)
+        filename = os.path.join(args.inputDir, file)
         df = pd.read_csv(filename, header=7, index_col=0)
         df['filename'] = file
+        info = fileparse(file)
         for key in info:
             df[key] = info[key]
+
+        dev = stdDev(df['Total Intensity'])
+
+        if args.appended:
+            for key in dev:
+                df[key] = dev[key]
+        elif args.separated:
+            dev['posX'] = info['posX']
+            dev['posY'] = info['posY']
+            std = std.append(dev, ignore_index=True)
+            pass
+
         dfs.append(df)
 
 mega = pd.concat(dfs)
 
-if not os.path.exists('megacsv'):
-    os.mkdir('megacsv')
+if not os.path.exists('outputs'):
+    os.mkdir('outputs')
 
-mega.to_csv('megacsv/output.csv')
-print('done')
+now = datetime.datetime.now()
+if args.appended:
+    mega.to_csv('outputs/EmitterDataStdDev-' + now.strftime('%m-%d--%H-%M-%S') + '.csv')
+else:
+    mega.to_csv('outputs/EmitterData-' + now.strftime('%m-%d--%H-%M-%S') + '.csv')
+
+if args.separated:
+    std.to_csv('outputs/StdDev-' + now.strftime('%m-%d--%H-%M-%S') + '.csv', index=False)
